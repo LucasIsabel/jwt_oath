b1d28824349e4c7387bff1a916ead68b873b788e
Lucas Isabel <lucas.de.o.isabel@accenture.com>
2018-10-08 12:07:11 -0300

    added authService as controller dependency

--- src/controlers/AuthController.js
+++ src/controlers/AuthController.js
@@ -1,16 +1,19 @@
 import AuthService from '../services/AuthService'
 import Status from '../glogal/enum'
 
-const { OK } = Status.httpStatus;
-const authService = new AuthService();
+const {OK} = Status.httpStatus;
 
 class AuthController {
 
-    async sendAnswer(req, res, next) {
-        // this method was added just to test
-        const result = await authService.verifiyAuthentatication();
-        res.status(OK).send(result);
-    }
+  constructor() {
+    this.authService = new AuthService();
+  }
+
+  async sendAnswer(req, res, next) {
+    // this method was added just to test
+    const result = await this.authService.verifiyAuthentatication();
+    res.status(OK).send(result);
+  }
 
 }
 
